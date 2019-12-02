7ff7acd79d7f51fbb908fc3bdea388583fdce0dd
Joyce Robbins <jtr13@columbia.edu>
2019-12-02 16:02:36 -0500

    render pdf, minor edits

---
+++ sandbox/latethanksgiving.R
@@ -0,0 +1,8 @@
+library(tis)
+library(tidyverse)
+h <- holidays(2000:2020)
+df <- data.frame(holiday = names(h), h) %>%
+  mutate(date = as.Date(as.character(h), format = "%Y%m%d")) %>%
+  filter(holiday == "Thanksgiving")
+
+ggplot(df, aes(tis::year(date), tis::day(date))) + geom_line(color="orange", lwd=1.5) +geom_point() + geom_vline(xintercept = 2019, color = "red") + scale_y_continuous(breaks=22:28)
