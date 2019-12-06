ce21990f433a3c6e6530af378f0e6644f3da28da
Joyce Robbins <jtr13@columbia.edu>
2019-12-05 20:01:52 -0500

    mostly welcome chapter

--- _common.R
+++ _common.R
@@ -13,6 +13,7 @@ makesvg <- function(name = "svg", width = 300, height = 200) {
 
 knitr::opts_chunk$set(
   echo = TRUE,
+  fig.align = 'center',
   comment="## R output ## ",
   message=FALSE,
   warning=FALSE
